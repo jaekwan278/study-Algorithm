09c385351c6170650b4de8aabd47db059175c304
Jaekwan <wprhks536@gmail.com>
2026-01-03 16:56:10 +0900

    Programmers-Graph : 가장 먼 노드

--- src/programmers/binarySearch/steppingStones/SteppingStones.java
+++ src/programmers/binarySearch/steppingStones/SteppingStones.java
@@ -10,53 +10,56 @@ package programmers.binarySearch.steppingStones;
 
  */
 
-import com.sun.source.tree.Tree;
-
-import java.util.*;
+import java.util.Arrays;
 
 public class SteppingStones {
 
     public int solution(int distance, int[] rocks, int n) {
         int answer = 0;
 
-        if(n == rocks.length) return distance;
+        if(n >= rocks.length) return distance;
 
         Arrays.sort(rocks);
+
         int[] tmp = Arrays.copyOfRange(rocks, 0, rocks.length+1);
         tmp[tmp.length-1] = distance;
 
-        System.out.println(Arrays.toString(tmp));
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        int min = 0;
-        for(int i = 0; i < tmp.length; i++){
-            map.put(i, tmp[i] - min);
-            min = tmp[i];
-        }
-
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort((e1, e2) -> e1.getValue() - e2.getValue());
-
-        for(int i = 0; i < n; i++){
-            map.get(0);
-        }
-
-        return answer;
+        return binarySearch(n, tmp);
     }
 
-    private int binarySearch(int distance, int n, int[] rocks){
-        int min = 0;
-        int max = distance;
+    private int binarySearch(int n, int[] tmp){
+        int result = 0;
+
+        int min = 1;
+        int max = tmp[tmp.length-1];
 
         while(min <= max){
             int mid = (min + max) / 2;
+            int cnt = 0;
 
+            int prev = 0;
+            for(int t : tmp){
+                if(mid <= t - prev){
+                    prev = t;
+                }else{
+                    cnt++;
+                }
+            }
 
+            if(cnt > n){
+                max = mid - 1;
+            }else{
+                min = mid + 1;
+                result = mid;
+            }
         }
 
-        return 0;
+        return result;
     }
 
+
+    // 2, 11, 14, 17, 21, 25
+    // 2, 9, 3, 3, 4, 4
 
     public static void main(String[] args) {
         SteppingStones task = new SteppingStones();
---
+++ src/programmers/graph/farthestNode/FarthestNode.java
@@ -0,0 +1,12 @@
+package programmers.graph.farthestNode;
+
+// 가장 먼 노드
+
+public class FarthestNode {
+
+    public int solution(int n, int[][] edge) {
+        int answer = 0;
+        return answer;
+    }
+
+}
