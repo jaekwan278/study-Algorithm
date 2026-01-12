4595b766f70b7830eb6c95980010ef750a38fbc8
Jaekwan <wprhks536@gmail.com>
2026-01-12 17:56:24 +0900

    Programers-PCCE : 심폐소생술

---
+++ src/programmers/pcce/cpr/CPR.java
@@ -0,0 +1,44 @@
+package programmers.pcce.cpr;
+
+// 심폐소생술
+
+/*
+
+public int[] solution(String[] cpr) {
+        int[] answer = {0, 0, 0, 0, 0};
+        String[] basic_order = {"check", "call", "pressure", "respiration", "repeat"};
+
+        for(int i=0; i<
+; i++){
+            for(int j=0; j<
+; j++){
+                if(cpr[i].equals(basic_order[j])){
+                    answer[i] =
+;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+ */
+
+public class CPR {
+
+    public int[] solution(String[] cpr) {
+        int[] answer = {0, 0, 0, 0, 0};
+        String[] basic_order = {"check", "call", "pressure", "respiration", "repeat"};
+
+        for(int i=0; i < cpr.length; i++){
+            for(int j=0; j < basic_order.length; j++){
+                if(cpr[i].equals(basic_order[j])){
+                    answer[i] = j+1;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+}
