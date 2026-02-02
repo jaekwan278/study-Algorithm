43907d5d6b0f1a53d8ee337d2448f303dfbcb5a0
Jaekwan <wprhks536@gmail.com>
2026-02-02 20:58:48 +0900

    Programmers-PCCE : 출력

---
+++ src/programmers/pcce/print/Print.java
@@ -0,0 +1,42 @@
+package programmers.pcce.print;
+
+// 출력
+
+/*
+
+    Spring is beginning
+    13
+    310
+
+ */
+
+/*
+
+public static void main(String[] args) {
+        String msg =
+                ;
+        int val1 =
+                ;
+        String val2 =
+                ;
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+
+ */
+
+public class Print {
+
+    public static void main(String[] args) {
+        String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+
+}
