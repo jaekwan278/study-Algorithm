c6b2cf3279d09fc7960c1cd80b9b9cf18ecf1c2b
Jaekwan <wprhks536@gmail.com>
2026-02-05 18:49:21 +0900

    Programmers-PCCE : 문자 출력

---
+++ src/programmers/pcce/printString/PrintString.java
@@ -0,0 +1,39 @@
+package programmers.pcce.printString;
+
+// 문자 출력
+
+/*
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String message = "
+";
+
+        System.out.println("3
+2
+1");
+        System.out.println(message);
+    }
+}
+
+ */
+
+/*
+
+3
+2
+1
+Let's go!
+
+ */
+
+public class PrintString {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
