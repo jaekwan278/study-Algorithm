db98ea4d8ad13842bfcc9caec10d82c30ed0ea5f
Jaekwan <wprhks536@gmail.com>
2026-02-13 15:02:03 +0900

    Programmers-SummerWinter2018 : 점프와 순간 이동

--- src/programmers/summerWinter2018/jumpAndTeleport/JumpAndTeleport.java
+++ src/programmers/summerWinter2018/jumpAndTeleport/JumpAndTeleport.java
@@ -1,4 +1,32 @@
 package programmers.summerWinter2018.jumpAndTeleport;
 
 public class JumpAndTeleport {
+
+    public int solution(int n) {
+        int ans = 0;
+
+        while(n > 0){
+            if(n % 2 == 1){
+                n -= 1;
+                ans++;
+            }else{
+                n /= 2;
+            }
+        }
+
+        return ans;
+    }
+
+    public int solution2(int n){
+        System.out.println(Integer.toBinaryString(n));
+        return Integer.bitCount(n);
+    }
+
+    public static void main(String[] args) {
+        JumpAndTeleport task = new JumpAndTeleport();
+        int n = 5000;
+
+        System.out.println(task.solution2(n));
+    }
+
 }
