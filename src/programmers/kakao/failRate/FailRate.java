bc2154f7264ef635a9bf6f0b6bec5c13aa72d72e
Jaekwan <wprhks536@gmail.com>
2026-04-02 13:36:50 +0900

    Programmers-Kako : 실패율

--- src/programmers/kakao/failRate/FailRate.java
+++ src/programmers/kakao/failRate/FailRate.java
@@ -7,59 +7,34 @@ import java.util.Arrays;
 public class FailRate {
 
     public int[] solution(int N, int[] stages) {
-        Integer[] boxed = new Integer[N];
-        for(int i = 1; i <= N; i++) boxed[i-1] = i;
-
-        float[] rate = new float[N+1];
+        int[] failCnt = new int[N + 2];
         for(int cur : stages){
-            if(cur > N) continue;
-            rate[cur]++;
+            failCnt[cur]++;
         }
 
-        int len = stages.length;
-        for(int i = 1; i < rate.length; i++){
-            float cnt = rate[i];
-            rate[i] /= len;
-            len -= (int)cnt;
-        }
+        int length = stages.length;
+        double[][] rateArr = new double[N][2];
 
-        Arrays.sort(boxed, (a, b) -> Float.compare(rate[b], rate[a]));
-
-        return Arrays.stream(boxed).mapToInt(Integer::intValue).toArray();
-    }
-
-    public int[] solution2(int N, int[] stages) {
-
-        int[] failCount = new int[N + 2];
-        for (int s : stages) {
-            failCount[s]++;
-        }
-
-        int remaining = stages.length;
-
-        double[][] arr = new double[N][2];
-        // [][0] = stage, [][1] = failure rate
-
-        for (int i = 1; i <= N; i++) {
-            if (remaining == 0) {
-                arr[i - 1][0] = i;
-                arr[i - 1][1] = 0;
+        for(int i = 1; i <= N; i++){
+            if(length == 0){
+                rateArr[i-1][0] = i;
+                rateArr[i-1][1] = 0;
                 continue;
             }
 
-            double rate = (double) failCount[i] / remaining;
-            arr[i - 1][0] = i;
-            arr[i - 1][1] = rate;
+            double rate = (double)failCnt[i] / length;
+            rateArr[i-1][0] = i;
+            rateArr[i-1][1] = rate;
 
-            remaining -= failCount[i];
+            length -= failCnt[i];
         }
 
-        Arrays.sort(arr, (a, b) -> {
-            if (b[1] == a[1]) return Double.compare(a[0], b[0]); // tie 처리
+        Arrays.sort(rateArr, (a, b) -> {
+            if(b[1] == a[1]) return Double.compare(a[0], b[0]);
             return Double.compare(b[1], a[1]);
         });
 
-        return Arrays.stream(arr).mapToInt(a -> (int)a[0]).toArray();
+        return Arrays.stream(rateArr).mapToInt(a -> (int)a[0]).toArray();
     }
 
     public static void main(String[] args) {
