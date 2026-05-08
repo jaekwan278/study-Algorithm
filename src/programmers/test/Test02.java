616549b2f51119759737ab25ded5067734bc5a2c
Jaekwan <wprhks536@gmail.com>
2026-05-08 19:53:47 +0900

    .

--- src/programmers/test/Test02.java
+++ src/programmers/test/Test02.java
@@ -1,19 +1,24 @@
 package programmers.test;
 
 public class Test02 {
-    public static void main(String[] args) {
-        double target = 10;
-        double compare = 188;
-        String sign = "";
 
-        if(target == 0 && compare == 0){
-            System.out.println("+0%");
-        }else{
-            if(target >= compare){
-                sign = "+";
-            }
+    public int solution(String t, String p) {
+        int answer = 0;
 
-            System.out.println(sign + String.format("%.1f", (((target - compare) / compare) * 100)) + "%");
+        for(int i = 0; i <= t.length() - p.length(); i++){
+            String sub = t.substring(i, i + p.length());
+
+            if(sub.compareTo(p) <= 0) answer++;
         }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Test02 task = new Test02();
+
+        String t = "10203", p = "15";
+
+        System.out.println(task.solution(t, p));
     }
 }
